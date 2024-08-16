55a8dee68fcf25957b814271fdb20abdf0629753
IvambergSilva <ivambergisilva@gmail.com>
2024-08-15 22:08:38 -0300

    feat: add format-currency file

---
+++ src/helps/format-currency.ts
@@ -0,0 +1,7 @@
+export const formatCurrency = (value: number) => {
+    return `R$ ${Intl.NumberFormat("pt-BR", {
+        currency: 'BRL',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    }).format(value)}`
+}
