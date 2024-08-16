33cfa49b5bb935afa365128d707cd080baf8e30e
IvambergSilva <ivambergisilva@gmail.com>
2024-08-15 22:05:41 -0300

    feat: add components to main page

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,21 @@
+import { ThemeProvider } from "styled-components";
+import { CartContextProvider } from "./context/cart-context";
+import { DefaultTheme } from "./styles/theme-default";
+import { Router } from "./router";
+import { BrowserRouter } from "react-router-dom";
+import { GlobalStyles } from "./styles/global-styles";
+
 export function App() {
     return (
-        <>CentralPerk</>
+        <>
+            <ThemeProvider theme={DefaultTheme}>
+                <BrowserRouter>
+                    <CartContextProvider>
+                        <Router />
+                    </CartContextProvider>
+                </BrowserRouter>
+                <GlobalStyles />
+            </ThemeProvider>
+        </>
     )
 }
